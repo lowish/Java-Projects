38fb5fb55ce70f752543565f3a860c5f5d527070
Prince William Tan <lowishxx@gmail.com>
2025-11-23 22:40:42 +0800

    first commit

---
+++ array.java
@@ -0,0 +1,6 @@
+public class day{
+    public static void main (String[] args){
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        System.out.print(days[0]); 
+    }
+}
