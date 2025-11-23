19e6a088e8e3e76a133f51ebaa2aa48c64a8702e
Prince William Tan <lowishxx@gmail.com>
2025-11-23 22:40:42 +0800

    added code in Method.java

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+    public static void main(String[] args) {
+        String message = "Progdats";
+        int Number = 42;
+        System.out.println("Hello, World!");
+        System.out.println("Number: " + Number);
+    }
+}
