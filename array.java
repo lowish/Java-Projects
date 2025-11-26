33c3ffbaf58823442b49303b2bd0cacdd1375c4f
Prince William Tan <lowishxx@gmail.com>
2025-11-27 00:21:50 +0800

    Delete array.java

--- array.java
+++
@@ -1,6 +0,0 @@
-public class day{
-    public static void main (String[] args){
-        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-        System.out.print(days[0]); 
-    }
-}
