a0cf7623b589e09c0c89603585dc110d4e9d619f
Prince William Tan <lowishxx@gmail.com>
2026-01-15 22:23:00 +0800

    Add Stringproblem file

---
+++ Activities/Stringproblem.java
@@ -0,0 +1,13 @@
+public class Stringproblem{
+public static void main(String[] args){
+String s1 = new String ("Java");
+String s2 = new String ("Java");
+
+if (s1.equals(s2)){
+System.out.println("same");
+}else{
+System.out.println("not same");
+}
+
+}
+}
