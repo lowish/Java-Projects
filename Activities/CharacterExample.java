12594634ab174c5b5acda5ac6368a4af57d49f68
Prince William Tan <lowishxx@gmail.com>
2026-02-09 23:58:35 +0800

    Add code files in Activities

---
+++ Activities/CharacterExample.java
@@ -0,0 +1,11 @@
+public class CharacterExample{
+public static void main(String[] args){
+
+char testch = 'A';
+
+System.out.println(Character.isLetter(testch));
+System.out.println(Character.isDigit(testch));
+System.out.print(Character.toLowerCase(testch));
+
+}
+}
