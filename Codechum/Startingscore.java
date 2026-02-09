a7ef3cdc2bf563ee9b6cec3515f37c5b739d03d3
Prince William Tan <lowishxx@gmail.com>
2026-02-09 23:53:21 +0800

    Add code files in Codechum

---
+++ Codechum/Startingscore.java
@@ -0,0 +1,14 @@
+public class Startingscore {
+
+    // Function declaration
+    public static int getStartingScore() {
+        // Your code here: Return the starting score
+        return 100;
+    }
+
+    public static void main(String[] args) {
+        // Call the function and store its return value
+        int score = getStartingScore();
+        System.out.println("Starting game score: " + score);
+    }
+}
