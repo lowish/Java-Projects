c73b598054f211b9b10c08802bff13862f4c8407
Prince William Tan <lowishxx@gmail.com>
2026-02-17 23:40:16 +0800

    Add ShoppingManager Updates

--- Codechum/ShoppingManager.java
+++ Codechum/ShoppingManager.java
@@ -6,7 +6,7 @@ public class ShoppingManager {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> shoppingList = new ArrayList<>();
 
-        // Input 3 items
+
         System.out.print("Enter item 1: ");
         shoppingList.add(scanner.nextLine());
 
@@ -16,17 +16,15 @@ public class ShoppingManager {
         System.out.print("Enter item 3: ");
         shoppingList.add(scanner.nextLine());
 
-        // Display shopping list
         System.out.println("Your shopping list: " + shoppingList);
 
-        // Ask which item to remove
+
         System.out.print("Which item to remove? ");
         String removeItem = scanner.nextLine();
 
-        // Remove item
+
         shoppingList.remove(removeItem);
 
-        // Display updated list
         System.out.println("Updated shopping list: " + shoppingList);
 
         scanner.close();
