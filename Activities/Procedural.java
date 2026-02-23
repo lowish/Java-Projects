e1635a442836d7743afe661f866a292fb273b1d0
Prince William Tan <lowishxx@gmail.com>
2026-02-24 00:20:51 +0800

    Add Procedural

---
+++ Activities/Procedural.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Procedural {
+public static double calculateArea(double length, double width) {
+return length * width;
+}
+
+public static void displayArea(double area) {
+System.out.println("The area of the rectangle is: " + area);
+}
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+System.out.println("Enter the length of the rectangle: ");
+double length = scanner.nextDouble();
+
+System.out.println("Enter the width of the rectangle: ");
+double width = scanner.nextDouble();
+
+double area = calculateArea(length, width);
+
+displayArea(area);
+scanner.close();
+}
+}
