2d0d58233015c613ff1f9f5cc324b8ca00380b45
Prince William Tan <lowishxx@gmail.com>
2026-02-26 23:18:54 +0800

    Add Arithmetic Encapsulation

---
+++ Activities/Arithmetic.java
@@ -0,0 +1,33 @@
+public class Arithmetic {
+
+private double num1;
+private double num2;
+
+public Arithmetic(double num1, double num2) {
+this.num1 = num1;
+this.num2 = num2;
+
+}
+
+public double add() {
+return num1 + num2;
+}
+
+public double subtract() {
+return num1 - num2;
+}
+
+public double multiply() {
+return num1 * num2;
+}
+
+public double divide() {
+if (num2 != 0) {
+return num1 / num2;
+} else {
+System.out.print("Invalid");
+return 0;
+}
+}
+
+}
