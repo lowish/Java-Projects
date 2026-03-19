097192739ce2f9c3e20d57655ff27f69e0af8e1f
Prince William Tan <lowishxx@gmail.com>
2026-03-20 02:03:19 +0800

    Add Superhero

---
+++ OOP/Constructors/Superhero.java
@@ -0,0 +1,13 @@
+package OOP.Constructors;
+
+public class Superhero {
+    public String name;
+    public String primaryPower;
+    
+    Superhero(String  name, String primaryPower) {
+        this.name =  name;
+        this.primaryPower = primaryPower;
+        
+    }
+
+}
