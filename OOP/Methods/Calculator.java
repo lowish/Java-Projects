e8354d8329389bbc1070aafb0d05fcde1aaa4cd4
Prince William Tan <lowishxx@gmail.com>
2026-03-23 23:10:36 +0800

    Add Calculator

---
+++ OOP/Methods/Calculator.java
@@ -0,0 +1,21 @@
+package OOP.Methods;
+
+public class Calculator {
+    // Implement the add, subtract, multiply, and divide methods here
+    
+    public int add(int a, int b){
+        return a + b;
+    }
+    
+    public int subtract(int a, int b){
+        return a - b;
+    }
+    
+    public int multiply(int a, int b){
+        return a * b;
+    }
+    
+    public double divide(double a, double b){
+        return a / b;
+    }
+}
