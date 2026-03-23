f170d40df13ba00bfbe53ecd8a0620e6c5d73475
Prince William Tan <lowishxx@gmail.com>
2026-03-23 22:37:34 +0800

    Update changes

--- OOP/Inheritance/Father.java
+++ OOP/Inheritance/Father.java
@@ -1,5 +1,16 @@
 package OOP.Inheritance;
 
-public class Father {
+public class Father extends Person {
+    public Father(String name, int age){
+        super(name, age, 'M');
+    }
     
+    public void introduceWithStyle(int n) {
+        for (int i = 0; i < n; i++){
+            for (int j = 0;  j < i; j++){
+                System.out.print(" ");
+            }
+            System.out.println("I am your father");
+        }
+    }
 }
