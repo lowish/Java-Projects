477d9f19a0513f3bff6b1248c37d74b03c925933
Prince William Tan <lowishxx@gmail.com>
2026-06-29 22:34:14 +0800

    Add Bird

---
+++ Codechum/Constructors/Bird.java
@@ -0,0 +1,15 @@
+class Bird {
+    
+    public String breed;
+    public boolean isNocturnal;
+    
+    public Bird(){
+        this.breed = "Unknown";
+        this.isNocturnal = false;
+    }
+    public Bird(String breed, boolean isNocturnal){
+            this.breed = breed;
+            this.isNocturnal = isNocturnal;
+
+        }
+}
