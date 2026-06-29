70cbf457a953403282fed945db3f66cf408afe1a
Prince William Tan <lowishxx@gmail.com>
2026-06-29 22:35:28 +0800

    Add Beverage

---
+++ Codechum/Constructors/Beverage.java
@@ -0,0 +1,17 @@
+class Beverage {
+    
+    public String flavor;
+    public String color;
+    
+    public Beverage(){
+        this.flavor = "Unknown";
+        this.color = "Unknown";
+            
+    }
+    public Beverage(String flavor, String color){
+            this.flavor = flavor;
+            this.color = color;
+            
+
+        }
+}
