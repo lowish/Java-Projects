a073f794a60b1d90aa65ee89e486276fcaac48f4
Prince William Tan <lowishxx@gmail.com>
2026-06-29 22:31:50 +0800

    Add Cat

---
+++ Codechum/Constructors/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    public String name;
+    public String color;
+    public char gender;
+    
+    public Cat(String name, String color, char gender){
+            this.name = name;
+            this.color = color;
+            this.gender = gender;
+        }
+    
+}
---
+++ Codechum/Constructors/CatMain.java
@@ -0,0 +1,14 @@
+public class CatMain {
+    
+    public static void main(String[] args){
+        
+    Cat myCat = new Cat("Whiskers", "Black", 'M');
+        
+        
+        System.out.println("Color: " + myCat.color);
+        System.out.println("Gender: " + myCat.gender);
+        System.out.println("Name: " + myCat.name);
+        
+        }
+    }
+    
